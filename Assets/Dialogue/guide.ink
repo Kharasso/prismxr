abcb82cbfc0d217512f400bda6c36fee8c7db753
Kharasso <airla88@gmail.com>
2024-04-19 23:31:36 -0400

    adds ink

--- Assets/Dialogue/guide.ink
+++ Assets/Dialogue/guide.ink
@@ -1,2 +1,42 @@
-This is a test
-That is also a test
+-> main
+
+=== main ===
+Welcome to our virtual exhibition, I'm Inanna, your virtual curator. I'm thrilled to guide you through this immersive journey into art and innovation.
+-> main2
+
+=== main2 ===
+How can I help you?
++ [Can you help me orient this exhibition?]
+    This exhibition consists of two main exhibtion halls separated by a division wall. On the east side is hall 1 and on the westside is hall 2. 
+    You can use the controller to teleport to wherever you want. Simply point to a location then hit the grip button.
+    + + [Do you have a map of this exhibition?]
+        Sure thing! Here is a map.
+        -> map
+    + + [Thank you. (back to main dialogue)]
+        -> main2
++ [I want to know more about the background of this exhibtion?]
+    -> intro
++ [I am good for now. Thank you!]
+    Of course! Feel free to reach out whenever you need help!
+    -> END
+
+
+=== map ===
+
+-> END
+
+=== intro ===
+This virtual sculpture exhibition is titled "Forms of Thought - where the timeless and the modern converge". 
+This collection features seminal works including Rodin's introspective masterpiece "The Thinker," the iconic grace of "Venus de Milo," and the majestic "Head of Helios," offering a unique exploration of human expression through form. 
+As you navigate through this digital space, each sculpture invites you to ponder the historical and aesthetic significance of these celebrated creations, providing a profound sensory and intellectual experience.
+-> intro2
+
+=== intro2 ===
+Do you need any further help?
++ [Can you give me some highlights of this exhibtion?]
+    Absolutely. At the center of the east hall is the Venus statue. Behind it by the wall is 'Head of Helios'. At the middle of the division wall is Rodin's thinker, and at the other end of the east hall is Napoleon. You will find some statues of animals in the west exhibtion hall.
+    -> intro
++ [Thank you. (back to main dialogue)]
+    -> main2
+    
+    
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ink.Runtime;
@@ -16,6 +16,9 @@ public class DialogueManager : MonoBehaviour
     private bool isPlaying;
     private VisualElement rootVisualElement;
     private Label dialogueLabel;
+    private Button continueButton;
+    private VisualElement choiceContainer;
+    private List<Button> choiceButtons;
 
     private void Awake()
     {
@@ -26,14 +29,27 @@ public class DialogueManager : MonoBehaviour
         instance = this;
 
         dialoguePanel = dialogueView.transform.Find("World Content").transform.Find("Flex Mesh").gameObject;
+        rootVisualElement = (VisualElement)dialogueView.GetComponent<FlexXRPanelManager>().flexXRPanelElements.FlexXRContentElement;
+        dialogueLabel = rootVisualElement.Q<Label>("DialogueLabel");
+        continueButton = rootVisualElement.Q<Button>("ContinueButton");
+        choiceContainer = rootVisualElement.Q<VisualElement>("ScrollChoices");
+        choiceButtons = new List<Button>();
+
+        for (int i = 0; i < choiceContainer.childCount; i++)
+        {
+            choiceButtons.Add(rootVisualElement.Q<Button>("Choice" + (i + 1).ToString()));
+            choiceButtons[i].style.display = DisplayStyle.None;
+        
+        }
+
     }
     // Start is called before the first frame update
     void Start()
     {
         isPlaying = false;
-        rootVisualElement = (VisualElement) dialogueView.GetComponent<FlexXRPanelManager>().flexXRPanelElements.FlexXRContentElement;
-        dialogueLabel = rootVisualElement.Q<Label>("DialogueLabel");
         dialoguePanel.SetActive(false);
+        continueButton.clicked += ContinueStory;
+        /*Debug.Log(choiceContainer.childCount);*/
     }
 
     // Update is called once per frame
@@ -81,10 +97,33 @@ public class DialogueManager : MonoBehaviour
         if (currStory.canContinue)
         {
             dialogueLabel.text = currStory.Continue();
+            ShowChoices();
         }
         else
         {
             ExitDialogue();
         }
+    }
+
+    private void ShowChoices()
+    {
+        List<Choice> choices = currStory.currentChoices;
+
+        if (choices.Count > choiceButtons.Count)
+        {
+            Debug.LogError("There are more choices than choice buttons.");
+        }
+
+        for (int i = 0;  i < choices.Count; i++)
+        {
+            choiceButtons[i].style.display = DisplayStyle.Flex;
+            choiceButtons[i].text = choices[i].text;
+        }
+
+        for (int i = choices.Count; i < choiceButtons.Count; i++)
+        {
+            choiceButtons[i].style.display = DisplayStyle.None;
+        }
+
     }
 }
